f746fd6dec70874b958a41c49000a4b07a53bab3
Bart Kardol <b.kardol@hotmail.com>
2021-12-01 18:50:32 +0100

    getting ready for day2

---
+++ src/2021/Day2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Day2
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            var input = InputService.InputService.GetInput();
+
+            Part1(input);
+            Part2(input);
+
+            Console.ReadLine();
+        }
+
+        static void Part1(string[] input)
+        {
+            Console.WriteLine();
+            Console.WriteLine("PART 1");
+
+            Console.WriteLine($"TODO");
+        }
+
+        static void Part2(string[] input)
+        {
+            Console.WriteLine();
+            Console.WriteLine("PART 2");
+
+            Console.WriteLine($"TODO");
+        }
+    }
+}
