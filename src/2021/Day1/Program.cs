5da4cd18d443670dec4169234a476e7a912d44b7
Bart Kardol <b.kardol@hotmail.com>
2021-12-01 18:36:35 +0100

    setup and day1

---
+++ src/2021/Day1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Day1
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            var depths = InputService.InputService.GetInput().Select(line => Convert.ToInt32(line)).ToArray();
+
+            Part1(depths);
+            Part2(depths);
+
+            Console.ReadLine();
+        }
+
+        static void Part1(int[] depths)
+        {
+            Console.WriteLine();
+            Console.WriteLine("PART 1");
+            var increasedDepths = GetIncreasedDepths(depths);
+            Console.WriteLine($"There are {increasedDepths} measurements that are larger than the previous measurement.");
+        }
+
+        static void Part2(int[] depths)
+        {
+            Console.WriteLine();
+            Console.WriteLine("PART 2");
+            var increasedWindowedDepths = GetIncreasedDepths(depths.Skip(2).Select((depth, i) => depth + depths[i] + depths[i + 1]).ToArray());
+            Console.WriteLine($"There are {increasedWindowedDepths} sums that are larger than the previous sum.");
+        }
+
+        static int GetIncreasedDepths(int[] depths) => depths.Where((depth, i) => i > 0 && depth > depths[i - 1]).Count();
+    }
+}
---
+++ src/InputService/InputService.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace InputService
+{
+    public static class InputService
+    {
+        public static string[] GetExample() => GetFileLines("example");
+
+        public static string[] GetInput() => GetFileLines("input");
+
+        private static string[] GetFileLines(string fileName) => File.ReadAllLines(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), $"{fileName}.aoc"));
+    }
+}
