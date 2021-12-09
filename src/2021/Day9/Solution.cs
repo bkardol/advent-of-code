b18492b32cf12e13cf12af8f8304e7d68cf8f5a8
Bart Kardol <b.kardol@hotmail.com>
2021-12-09 08:13:50 +0100

    day 9 - improved

--- src/2021/Day9/Solution.cs
+++ src/2021/Day9/Solution.cs
@@ -1,89 +1,87 @@
 ﻿namespace Day9
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
     using Common;
     using Common.Extensions;
 
-    internal record Location(int I, int J);
-
-    internal class Solution : PuzzleSolution<int[][]>
+    internal class Solution : PuzzleSolution<Location[][]>
     {
-        public override int[][] ParseInput(string[] lines) => lines.Select(l => l.ToIntArray()).ToArray();
+        public override Location[][] ParseInput(string[] lines)
+        {
+
+            var cave = lines.Select(l => l.ToIntArray().Select((height) => new Location(height)).ToArray()).ToArray();
+
+            for (int y = 0; y < cave.Length; ++y)
+            {
+                var row = cave[y];
+                for (int x = 0; x < row.Length; ++x)
+                {
+                    if(x > 0)
+                    {
+                        cave[y][x].SetLeft(row[x - 1]);
+                    }
+                    if (x < row.Length - 1)
+                    {
+                        cave[y][x].SetRight(row[x + 1]);
+                    }
+                    if (y > 0)
+                    {
+                        cave[y][x].SetTop(cave[y - 1][x]);
+                    }
+                    if (y < cave.Length - 1)
+                    {
+                        cave[y][x].SetBottom(cave[y + 1][x]);
+                    }
+                }
+            }
+
+            return cave;
+        }
 
         public override string[] Part1()
         {
-            int sumOfLowestPoints = GetLowestPointForBasin().Sum(point => Input[point.I][point.J] + 1);
+            int sumOfLowestPoints = GetCaveLowestPoints().Sum(location => location.Height + 1);
 
             return new string[]
             {
-                $"{sumOfLowestPoints}"
+                $"The sum of the lowest point in the cave is: {sumOfLowestPoints}"
             };
         }
 
         public override string[] Part2()
         {
-            int result = GetLowestPointForBasin().Select(point =>
+            int result = GetCaveLowestPoints().Select(location =>
             {
-                var current = Input[point.I][point.J];
-                var basin = new Dictionary<Location, int> { [point] = current };
-                GetAdjacentHigherPoints(point.I, point.J, current, basin);
+                var basin = new Dictionary<Location, int> { [location] = location.Height };
+                GetAdjacentHigherPoints(location, basin);
                 return basin.Count;
             }).OrderByDescending(size => size).Take(3).Aggregate(1, (a, b) => a * b);
 
             return new string[]
             {
-                $"{result}"
+                $"The size of the 3 largest basins multiplied is: {result}"
             };
         }
 
-        public List<Location> GetLowestPointForBasin()
+        public IEnumerable<Location> GetCaveLowestPoints() => Input.SelectMany(r => r.Where(location => location.GetAdjacentLocations().All(adjacent => location.Height < adjacent.Height)));
+
+        public void GetAdjacentHigherPoint(Location currentLocation, Location adjacentLocation, Dictionary<Location, int> basin)
         {
-            var locations = new List<Location>();
-            for (int i = 0; i < Input.Length; ++i)
+            if (adjacentLocation.Height > currentLocation.Height &&
+                adjacentLocation.Height != 9 &&
+                !basin.ContainsKey(adjacentLocation))
             {
-                for (int j = 0; j < Input[i].Length; ++j)
-                {
-                    var current = Input[i][j];
-                    var left = j > 0 ? Input[i][j - 1] : int.MaxValue;
-                    var right = j < Input[i].Length - 1 ? Input[i][j + 1] : int.MaxValue;
-                    var top = i > 0 ? Input[i - 1][j] : int.MaxValue;
-                    var bottom = i < Input.Length - 1 ? Input[i + 1][j] : int.MaxValue;
-                    if (current < left && current < right && current < top && current < bottom)
-                    {
-                        locations.Add(new Location(i, j));
-                    }
-                }
+                GetAdjacentHigherPoints(adjacentLocation, basin);
             }
-            return locations;
         }
 
-        public void GetAdjacentHigherPoints(int i, int j, int current, Dictionary<Location, int> basin)
+        public void GetAdjacentHigherPoints(Location location, Dictionary<Location, int> basin)
         {
-            var left = j > 0 ? Input[i][j - 1] : int.MinValue;
-            var right = j < Input[i].Length - 1 ? Input[i][j + 1] : int.MinValue;
-            var top = i > 0 ? Input[i - 1][j] : int.MinValue;
-            var bottom = i < Input.Length - 1 ? Input[i + 1][j] : int.MinValue;
-            if (left > current && left != 9 && !basin.ContainsKey(new Location(i, j - 1)))
+            basin[location] = location.Height;
+            foreach (var adjacentLocation in location.GetAdjacentLocations())
             {
-                basin[new Location(i, j - 1)] = left;
-                GetAdjacentHigherPoints(i, j - 1, left, basin);
-            }
-            if (right > current && right != 9 && !basin.ContainsKey(new Location(i, j + 1)))
-            {
-                basin[new Location(i, j + 1)] = right;
-                GetAdjacentHigherPoints(i, j + 1, right, basin);
-            }
-            if (top > current && top != 9 && !basin.ContainsKey(new Location(i - 1, j)))
-            {
-                basin[new Location(i - 1, j)] = top;
-                GetAdjacentHigherPoints(i - 1, j, top, basin);
-            }
-            if (bottom > current && bottom != 9 && !basin.ContainsKey(new Location(i + 1, j)))
-            {
-                basin[new Location(i + 1, j)] = bottom;
-                GetAdjacentHigherPoints(i + 1, j, bottom, basin);
+                GetAdjacentHigherPoint(location, adjacentLocation, basin);
             }
         }
     }
