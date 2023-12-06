2c3d500d15fd2ddb5ec18d24ba75561d07d6bc3d
Bart Kardol <bart.kardol@essent.nl>
2023-12-06 06:31:36 +0100

    day 6

---
+++ src/2023/Day6/Race.cs
@@ -0,0 +1,36 @@
+﻿namespace Day6
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class Race
+    {
+        private readonly bool isPartOneRace;
+        private readonly long time;
+        private readonly long distance;
+
+        public Race(long time, long distance, bool isPartOneRace)
+        {
+            this.time = time;
+            this.distance = distance;
+            this.isPartOneRace = isPartOneRace; 
+        }
+
+        internal long GetNumberOfWaysToBeatRecord(bool partOne)
+        {
+            if (partOne != isPartOneRace) return 1;
+
+            long numberOfWaysToBeatRecord = 0;
+            for(long i = 1; i < time; i++)
+            {
+                if(i * (time - i) > distance)
+                    numberOfWaysToBeatRecord++;
+            }
+            Console.WriteLine($"- Race with a time of {time} and distance of {distance} has {numberOfWaysToBeatRecord} number of ways to beat the record");
+            return numberOfWaysToBeatRecord;
+        }
+    }
+}
---
+++ src/2023/Day6/Solution.cs
@@ -0,0 +1,37 @@
+﻿namespace Day6
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using Common;
+    using Common.IEnumerable;
+
+    internal class Solution : PuzzleSolution<Race[]>
+    {
+        public override Race[] ParseInput(string[] lines)
+        {
+            var times = lines[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Skip(1);
+            var distance = lines[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Skip(1);
+
+            var races = times.ToIntArray().Select((time, i) => new Race(time, distance.ToIntArray()[i], true)).ToList();
+            races.Add(new Race(long.Parse(Regex.Replace(lines[0], @"[^0-9]", "")), long.Parse(Regex.Replace(lines[1], @"[^0-9]", "")), false));
+
+            return races.ToArray();
+        }
+
+        public override string[] Part1() => getNumberOfWaysToWinMultiplied(true);
+
+        public override string[] Part2() => getNumberOfWaysToWinMultiplied(false);
+
+        private string[] getNumberOfWaysToWinMultiplied(bool partOne)
+        {
+            long waysToWinMultiplied = this.Input
+                .Select(race => race.GetNumberOfWaysToBeatRecord(partOne))
+                .Aggregate((long)1, (multiplied, next) => multiplied * next);
+            return new string[]
+            {
+                $"This means that the answer is: {waysToWinMultiplied}"
+            };
+        }
+    }
+}
