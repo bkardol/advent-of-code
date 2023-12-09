8b70e0629656138a32cd386a6bdebc617ee696b8
Bart Kardol <bart.kardol@essent.nl>
2023-12-09 10:04:10 +0100

    day 9

---
+++ src/2023/Day9/Sequence.cs
@@ -0,0 +1,38 @@
+﻿namespace Day9
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Sequence
+    {
+        private readonly long[] numbers;
+        private readonly List<long[]> runs = new List<long[]>();
+
+        public Sequence(long[] numbers)
+        {
+            this.numbers = numbers;
+            runs.Add(numbers);
+            while (runs.Count == 0 || runs.Last().Any(v => v != 0))
+            {
+                var increases = new List<long>();
+                var prevRun = runs.Last();
+                for (int i = 1; i < prevRun.Length; i++)
+                {
+                    increases.Add(prevRun[i] - prevRun[i - 1]);
+                }
+                runs.Add(increases.ToArray());
+            }
+        }
+
+        public long GetExtrapolated()
+        {
+            return runs.AsEnumerable().Reverse().Aggregate((long)0, (sum, run) => sum + run.Last());
+        }
+
+        public long GetExtrapolatedBackwards()
+        {
+            return runs.AsEnumerable().Reverse().Aggregate((long)0, (sum, run) => run.First() - sum);
+        }
+    }
+}
---
+++ src/2023/Day9/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace Day9
+{
+    using System;
+    using System.Linq;
+    using Common;
+    using Common.IEnumerable;
+
+    internal class Solution : PuzzleSolution<Sequence[]>
+    {
+        public override Sequence[] ParseInput(string[] lines) => lines.Select(line => new Sequence(line.Split(' ').ToLongArray())).ToArray();
+
+        public override string[] Part1()
+        {
+            var result = Input.Sum(sequence => sequence.GetExtrapolated());
+            return new string[]
+            {
+                result.ToString()
+            };
+        }
+
+        public override string[] Part2()
+        {
+            var result = Input.Sum(sequence => sequence.GetExtrapolatedBackwards());
+            return new string[]
+            {
+                result.ToString()
+            };
+        }
+    }
+}
