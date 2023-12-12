e15aa413d64fa680d0cfcc6ef91c0c3c4ec7a926
Bart Kardol <bart.kardol@essent.nl>
2023-12-13 00:13:10 +0100

    day 12 - optimized

---
+++ src/2023/Day12/Row.cs
@@ -0,0 +1,75 @@
+﻿namespace Day12
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Row
+    {
+        private string fullRow;
+        private int[] allDamagedSprings;
+        private static readonly Dictionary<(string row, string numbers), long> cache = new();
+
+        public Row(string row, int[] damagedSprings)
+        {
+            this.fullRow = row;
+            this.allDamagedSprings = damagedSprings;
+        }
+
+        public void Unfold()
+        {
+            fullRow = string.Join("?", Enumerable.Repeat(fullRow, 5));
+            allDamagedSprings = Enumerable.Repeat(allDamagedSprings, 5).SelectMany(springs => springs).ToArray();
+        }
+
+        public long GetNumberOfArrangements() => GetNumberOfArrangements(fullRow, allDamagedSprings);
+
+        private long GetNumberOfArrangements(string springs, int[] brokenSpringGroups)
+        {
+            var hasAnySprings = springs.Length > 0;
+            var hasAnyBrokenSpringGroups = brokenSpringGroups.Length > 0;
+            if (!hasAnySprings)
+            {
+                return hasAnyBrokenSpringGroups ? 0 : 1;
+            }
+            if (!hasAnyBrokenSpringGroups)
+            {
+                return ContainsBrokenSpring(springs) ? 0 : 1;
+            }
+
+            var cacheKey = (springs, string.Join(";", brokenSpringGroups));
+            if (cache.ContainsKey(cacheKey))
+            {
+                return cache[cacheKey];
+            }
+
+            long arrangements = 0;
+            var spring = springs[0];
+            if (IsPossiblyOperationalSpring(spring))
+            {
+                arrangements += GetNumberOfArrangements(springs[1..], brokenSpringGroups);
+            }
+
+            var damagedSpringCount = brokenSpringGroups[0];
+            if (IsPossiblyBrokenSpring(spring))
+            {
+                if (
+                    damagedSpringCount <= springs.Length &&
+                    !ContainsOperationalSpring(springs[..damagedSpringCount]) &&
+                    (damagedSpringCount == springs.Length || !IsBrokenSpring(springs[damagedSpringCount]))
+                )
+                {
+                    var newSprings = damagedSpringCount == springs.Length ? "" : springs[(damagedSpringCount + 1)..];
+                    arrangements += GetNumberOfArrangements(newSprings, brokenSpringGroups.Skip(1).ToArray());
+                }
+            }
+
+            return cache[cacheKey] = arrangements;
+        }
+
+        private static bool ContainsBrokenSpring(string springs) => springs.Contains('#');
+        private static bool ContainsOperationalSpring(string springs) => springs.Contains('.');
+        private static bool IsBrokenSpring(char spring) => spring == '#';
+        private static bool IsPossiblyBrokenSpring(char spring) => spring == '#' || spring == '?';
+        private static bool IsPossiblyOperationalSpring(char spring) => spring == '.' || spring == '?';
+    }
+}
---
+++ src/2023/Day12/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace Day12
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Common;
+    using Common.IEnumerable;
+
+    internal class Solution : PuzzleSolution<Row[]>
+    {
+        public override Row[] ParseInput(string[] lines) => lines.Select(line =>
+        {
+            var splitted = line.Split(' ');
+            return new Row(splitted[0], splitted[1].Split(',').ToIntArray());
+        }).ToArray();
+
+        public override string[] Part1()
+        {
+            var results = Input.Select(i => i.GetNumberOfArrangements());
+            return new string[]
+            {
+                results.Sum().ToString()
+            };
+        }
+
+        public override string[] Part2()
+        {
+            foreach (var input in Input)
+            {
+                input.Unfold();
+            }
+
+            var results = Input.Select(i => i.GetNumberOfArrangements()).ToArray();
+            return new string[]
+            {
+                results.Sum().ToString()
+            };
+        }
+    }
+}
