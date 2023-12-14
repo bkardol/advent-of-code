efcbcd0b78923f8f046fc63b4b4bb87da081aa0b
Bart Kardol <bart.kardol@essent.nl>
2023-12-14 07:10:58 +0100

    day 14

---
+++ src/2023/Day14/Location.cs
@@ -0,0 +1,30 @@
+﻿namespace Day14
+{
+    using System;
+    using Common.Matrix;
+
+    internal class Location : Cell<Location, LocationType>
+    {
+        public void SlideRock(Func<Location, Location> nextLocation)
+        {
+            if (this.Value == LocationType.RoundedRock)
+            {
+                var newSpot = this;
+                while (nextLocation(newSpot) != null && nextLocation(newSpot).Value == LocationType.EmptySpace)
+                {
+                    newSpot = nextLocation(newSpot);
+                }
+                if (newSpot != this)
+                {
+                    this.SwapPosition(newSpot);
+                }
+            }
+        }
+        public void SlideRockNorth() => SlideRock((location) => location.Top);
+        public void SlideRockEast() => SlideRock((location) => location.Right);
+        public void SlideRockSouth() => SlideRock((location) => location.Bottom);
+        public void SlideRockWest() => SlideRock((location) => location.Left);
+
+        private void SwapPosition(Location location) => (location.Value, this.Value) = (this.Value, location.Value);
+    }
+}
---
+++ src/2023/Day14/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Day14
+{
+    using Common;
+
+    public class Program
+    {
+        public static void Main() => PuzzleProgram.Main();
+    }
+}
