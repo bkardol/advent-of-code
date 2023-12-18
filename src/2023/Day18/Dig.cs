a477cffb8847e9913cd9147658e34dee62f19f56
Bart Kardol <bart.kardol@essent.nl>
2023-12-18 12:25:07 +0100

    day 18 - full speed

--- src/2023/Day18/Dig.cs
+++ src/2023/Day18/Dig.cs
@@ -3,128 +3,33 @@
     using System;
     using System.Collections.Generic;
 
-    internal struct DigInstruction
+    internal readonly struct DigInstruction
     {
         private readonly Direction direction;
-        private readonly Direction trueDirection;
         private readonly int cubes;
-        private readonly int trueCubes;
 
-        public DigInstruction(Direction direction, int cubes, string color)
+        public DigInstruction(Direction direction, int cubes)
         {
             this.direction = direction;
-            this.trueDirection = GetTrueDirection(color);
             this.cubes = cubes;
-            this.trueCubes = GetTrueCubes(color);
+        }
+
+        public DigInstruction(string color)
+        {
+            this.direction = GetTrueDirection(color);
+            this.cubes = GetTrueCubes(color);
         }
 
         public (long X, long Y, long distance) CompleteInstruction(long currentX, long currentY)
         {
-            return trueDirection switch
+            return direction switch
             {
-                Direction.Up => (currentX, currentY - trueCubes, trueCubes),
-                Direction.Down => (currentX, currentY + trueCubes, trueCubes),
-                Direction.Left => (currentX - trueCubes, currentY, trueCubes),
-                Direction.Right => (currentX + trueCubes, currentY, trueCubes),
+                Direction.Up => (currentX, currentY - cubes, cubes),
+                Direction.Down => (currentX, currentY + cubes, cubes),
+                Direction.Left => (currentX - cubes, currentY, cubes),
+                Direction.Right => (currentX + cubes, currentY, cubes),
                 _ => throw new ArgumentException("Invalid direction."),
             };
-        }
-
-        public (int x, int y) CompleteInstruction(List<List<Location>> digsite, int currentX, int currentY)
-        {
-            for (int digs = 0; digs < cubes; digs++)
-            {
-                List<Location> row;
-                switch (direction)
-                {
-                    case Direction.Up:
-                        if (currentY > 0)
-                        {
-                            row = digsite[currentY - 1];
-                            row[currentX].Dig();
-                            currentY--;
-                        }
-                        else
-                        {
-                            row = new List<Location>();
-                            for (int i = 0; i < digsite[0].Count; i++)
-                            {
-                                var location = new Location();
-                                if (i == currentX)
-                                {
-                                    location.Dig();
-                                }
-                                row.Add(location);
-                            }
-                            digsite.Insert(currentY, row);
-                        }
-                        break;
-                    case Direction.Down:
-                        if (currentY < digsite.Count - 1)
-                        {
-                            row = digsite[currentY + 1];
-                            row[currentX].Dig();
-                        }
-                        else
-                        {
-                            row = new List<Location>();
-                            for (int i = 0; i < digsite[0].Count; i++)
-                            {
-                                var location = new Location();
-                                if (i == currentX)
-                                {
-                                    location.Dig();
-                                }
-                                row.Add(location);
-                            }
-                            digsite.Add(row);
-                        }
-                        currentY++;
-                        break;
-                    case Direction.Left:
-                        row = digsite[currentY];
-                        if (currentX > 0)
-                        {
-                            row[currentX - 1].Dig();
-                            currentX--;
-                        }
-                        else
-                        {
-                            for (int i = 0; i < digsite.Count; i++)
-                            {
-                                var location = new Location();
-                                if (i == currentY)
-                                {
-                                    location.Dig();
-                                }
-                                digsite[i].Insert(0, location);
-                            }
-                        }
-                        break;
-                    case Direction.Right:
-                        row = digsite[currentY];
-                        if (currentX < digsite[0].Count - 1)
-                        {
-                            row[currentX + 1].Dig();
-                        }
-                        else
-                        {
-                            for (int i = 0; i < digsite.Count; i++)
-                            {
-                                var location = new Location();
-                                if (i == currentY)
-                                {
-                                    location.Dig();
-                                }
-                                digsite[i].Add(location);
-                            }
-                        }
-                        currentX++;
-                        break;
-                }
-            }
-
-            return (currentX, currentY);
         }
 
         private static Direction GetTrueDirection(string color)
--- src/2023/Day18/Solution.cs
+++ src/2023/Day18/Solution.cs
@@ -2,10 +2,8 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.IO;
     using System.Linq;
     using Common;
-    using Common.IEnumerable;
     using Common.MathExtensions;
 
     internal class Solution : PuzzleSolution<DigInstruction[]>
@@ -13,44 +11,21 @@
         public override DigInstruction[] ParseInput(string[] lines) => lines.Select(line =>
         {
             var splitted = line.Split(new[] { ' ', '(', ')' }, StringSplitOptions.RemoveEmptyEntries);
-            return new DigInstruction((Direction)splitted[0][0], int.Parse(splitted[1]), splitted[2]);
+            if(Part == 1)
+            {
+                return new DigInstruction((Direction)splitted[0][0], int.Parse(splitted[1]));
+            }
+            else
+            {
+                return new DigInstruction(splitted[2]);
+            }
         }).ToArray();
 
-        public override string[] Part1()
-        {
-            var firstDig = new Location();
-            var digsite = new List<List<Location>> { new List<Location> { firstDig } };
+        public override string[] Part1() => new string[] { "The lagoon can hold this many cubic meters: " + GetLagoonSurfaceArea() };
 
-            var currentLocation = (0, 0);
-            foreach (var digInstruction in Input)
-            {
-                currentLocation = digInstruction.CompleteInstruction(digsite, currentLocation.Item1, currentLocation.Item2);
-            }
+        public override string[] Part2() => new string[] {"The lagoon can hold this many cubic meters: " + GetLagoonSurfaceArea() };
 
-            var matrixDigsite = digsite.ToMatrix<Location, int>();
-            var flattenedDigsite = digsite.SelectMany(l => l).ToArray();
-
-            var middleLocation = flattenedDigsite[flattenedDigsite.Length / 2];
-            Stack<Location> floodLocations = new Stack<Location>();
-            floodLocations.Push(middleLocation);
-            while (floodLocations.Count > 0)
-            {
-                var location = floodLocations.Pop();
-                location.Dig();
-                foreach (var l in location.GetAdjacentToFlood())
-                {
-                    floodLocations.Push(l);
-                }
-            }
-
-            File.WriteAllLines("output.aoc", matrixDigsite.Select(i => i.Aggregate("", (total, next) => $"{total}{(next.Value == 0 ? " " : next.Value.ToString())}")));
-            return new string[]
-            {
-                flattenedDigsite.Count(l => l.Value > 0).ToString()
-            };
-        }
-
-        public override string[] Part2()
+        private long GetLagoonSurfaceArea()
         {
             var coordinates = new List<(long X, long Y, long Distance)>();
             (long X, long Y, long Distance) currentCoordinate = (0, 0, 0);
@@ -59,11 +34,9 @@
                 currentCoordinate = digInstruction.CompleteInstruction(currentCoordinate.X, currentCoordinate.Y);
                 coordinates.Add(currentCoordinate);
             }
-            var result = CommonMath.CalculateSurfaceAreaWithShoelace(coordinates.ToArray());
-            return new string[]
-            {
-                result.ToString()
-            };
+
+            return CommonMath.CalculateSurfaceAreaWithShoelace(coordinates.ToArray());
+
         }
     }
 }
