d79511ce9271b5cc5e4b9bcb2c1d577d712c6714
Bart Kardol <bart.kardol@essent.nl>
2023-12-20 08:21:51 +0100

    day 20

---
+++ src/2023/Day20/Module.cs
@@ -0,0 +1,47 @@
+﻿namespace Day20
+{
+    internal class Module(string key, bool isFlipFlop, bool isConjunction, string[] destinations)
+    {
+        public string Key { get; } = key;
+        public bool IsFlipFlop { get; } = isFlipFlop;
+        public bool IsConjunction { get; } = isConjunction;
+        public string[] Destinations { get; } = destinations;
+
+        public bool IsOn { get; private set; }
+        public Dictionary<string, bool> ReceivedPulses { get; private set; } = [];
+
+        public bool? ProcessPulse(string source, bool isHighPulse)
+        {
+            if (Key == "roadcaster")
+            {
+                return isHighPulse;
+            }
+
+            if (IsFlipFlop)
+            {
+                if (!isHighPulse)
+                {
+                    IsOn = !IsOn;
+                    return IsOn;
+                }
+            }
+
+            if (IsConjunction)
+            {
+                ReceivedPulses[source] = isHighPulse;
+                return !ReceivedPulses.Values.All(p => p);
+            }
+
+            return null;
+        }
+
+        internal void SetPossibleSources(string[] sources)
+        {
+            if (!IsConjunction)
+            {
+                return;
+            }
+            ReceivedPulses = sources.ToDictionary(s => s, s => false);
+        }
+    }
+}
---
+++ src/2023/Day20/Solution.cs
@@ -0,0 +1,105 @@
+﻿using Common;
+using Common.MathExtensions;
+using Day20;
+
+internal class Solution : PuzzleSolution<Dictionary<string, Module>>
+{
+    private string rxConjunctionSource = "";
+
+    public override Dictionary<string, Module> ParseInput(string[] lines)
+    {
+        var dict = lines.Select(line =>
+        {
+            var splitted = line.Split(" -> ");
+            var isFlipFlop = splitted[0][0] == '%';
+            var isConjunction = splitted[0][0] == '&';
+            var module = new Module(splitted[0][1..], isFlipFlop, isConjunction, splitted[1].Split(", "));
+            if (module.Destinations.Contains("rx"))
+            {
+                rxConjunctionSource = module.Key;
+            }
+            return module;
+        }).ToDictionary(m => m.Key, m => m);
+
+        foreach (var key in dict.Keys)
+        {
+            var module = dict[key];
+            module.SetPossibleSources(dict.Values.Where(m => m.Destinations.Contains(key)).Select(m => m.Key).ToArray());
+        }
+
+        return dict;
+    }
+
+    public override string[] Part1()
+    {
+        List<bool> pulses = [];
+        Queue<(string source, string module, bool pulse)> instructions = new();
+        for (int i = 0; i < 1000; i++)
+        {
+            instructions.Enqueue(("button", "roadcaster", false));
+            while (instructions.Count > 0)
+            {
+                var instruction = instructions.Dequeue();
+                pulses.Add(instruction.pulse);
+                if (!Input.ContainsKey(instruction.module))
+                {
+                    continue;
+                }
+
+                var module = Input[instruction.module];
+                var isHighPulse = module.ProcessPulse(instruction.source, instruction.pulse);
+
+                if (isHighPulse != null)
+                {
+                    foreach (var dest in module.Destinations)
+                    {
+                        instructions.Enqueue((module.Key, dest, (bool)isHighPulse));
+                    }
+                }
+            }
+        }
+        return [(pulses.Count(p => p) * pulses.Count(p => !p)).ToString()];
+    }
+
+    public override string[] Part2()
+    {
+        Queue<(string source, string module, bool pulse)> instructions = new();
+        Dictionary<string, int> rxSources = Input.Values.Where(m => m.Destinations.Contains(rxConjunctionSource)).ToDictionary(m => m.Key, m => 0);
+        
+        for (int i = 1; ; i++)
+        {
+            instructions.Enqueue(("button", "roadcaster", false));
+            while (instructions.Count > 0)
+            {
+                var instruction = instructions.Dequeue();
+                if (!Input.ContainsKey(instruction.module))
+                {
+                    continue;
+                }
+
+                var module = Input[instruction.module];
+                var isHighPulse = module.ProcessPulse(instruction.source, instruction.pulse);
+
+                if (rxSources.TryGetValue(instruction.module, out int value) && value == 0 && isHighPulse == true)
+                {
+                    rxSources[instruction.module] = i;
+                }
+
+                if (isHighPulse != null)
+                {
+                    foreach (var dest in module.Destinations)
+                    {
+                        instructions.Enqueue((module.Key, dest, (bool)isHighPulse));
+                    }
+                }
+            }
+            if (rxSources.Values.All(highPulseIndex => highPulseIndex > 0))
+            {
+                break;
+            }
+        }
+
+        var result = rxSources.Values.Select(i => (long)i).Aggregate(CommonMath.LeastCommonMultiple);
+        return [result.ToString()];
+    }
+}
