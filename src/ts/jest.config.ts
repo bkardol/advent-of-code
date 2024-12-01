218a2ec4486311360fb09301b8c084f5873eb98d
Bart Kardol <bart.kardol@essent.nl>
2024-12-01 06:49:05 +0100

    esm project setup fixes

---
+++ src/ts/jest.config.ts
@@ -0,0 +1,21 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+import { pathsToModuleNameMapper } from "ts-jest";
+import { compilerOptions } from "./tsconfig.json";
+
+const config: JestConfigWithTsJest = {
+  verbose: true,
+  transform: {
+    "^.+\\.ts?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/",
+  }),
+};
+
+export default config;
