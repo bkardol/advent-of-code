26433d43a67393e5ebc8dbac3f48ffb411c22df5
Bart Kardol <bart.kardol@essent.nl>
2024-12-01 06:49:11 +0100

    day1

--- src/ts/2024/day1/solution.spec.ts
+++ src/ts/2024/day1/solution.spec.ts
@@ -11,22 +11,22 @@ describe("Day 1", () => {
   describe("part 1", () => {
     test("part 1 example", () => {
       const input = getExampleInput(__dirname);
-      expect(solution.part1(input)).toBe(0);
+      expect(solution.part1(input)).toBe(11);
     });
     test("part 1 input", () => {
       const input = getInput(__dirname);
-      expect(solution.part1(input)).toBe(0);
+      expect(solution.part1(input)).toBe(1151792);
     });
   });
 
   describe("part 2", () => {
     test("part 2 example", () => {
       const input = getExampleInput(__dirname);
-      expect(solution.part2(input)).toBe(0);
+      expect(solution.part2(input)).toBe(31);
     });
     test("part 2 input", () => {
       const input = getInput(__dirname);
-      expect(solution.part2(input)).toBe(0);
+      expect(solution.part2(input)).toBe(21790168);
     });
   });
 });
--- src/ts/2024/day1/solution.ts
+++ src/ts/2024/day1/solution.ts
@@ -1,9 +1,32 @@
+import { asStringArray } from "@aoc/common";
+
 export class Solution {
   public part1(input: string): number {
-    return 0;
+    const { left, right } = this.asLocationIds(input);
+
+    const output = left
+      .map((left, i) => Math.abs(left - right[i]))
+      .reduce((prev, curr) => prev + curr, 0);
+
+    return output;
   }
 
   public part2(input: string): number {
-    return 0;
+    const { left, right } = this.asLocationIds(input);
+
+    const output = left
+      .map((left) => left * right.filter((value) => value === left).length)
+      .reduce((prev, curr) => prev + curr, 0);
+    return output;
+  }
+
+  private asLocationIds(input: string) {
+    const values = asStringArray(input).map((line) =>
+      line.split("   ").map((value) => Number(value))
+    );
+    const left = values.sort((a, b) => a[0] - b[0]).map(([left]) => left);
+    const right = values.sort((a, b) => a[1] - b[1]).map(([, right]) => right);
+
+    return { left, right };
   }
 }
