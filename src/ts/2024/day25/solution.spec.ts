a9a5b187cee312c9058733b02c029919fc267027
Bart Kardol <bart.kardol@essent.nl>
2024-12-18 07:02:13 +0100

    boilerplate

---
+++ src/ts/2024/day25/solution.spec.ts
@@ -0,0 +1,32 @@
+import { getExampleInput, getInput } from "@aoc/common";
+import { Solution } from "./solution";
+
+describe("Day 25", () => {
+  let solution: Solution;
+
+  beforeEach(() => {
+    solution = new Solution();
+  });
+
+  describe("part 1", () => {
+    test("part 1 example", () => {
+      const input = getExampleInput(__dirname);
+      expect(solution.part1(input)).toBe(0);
+    });
+    test("part 1 input", () => {
+      const input = getInput(__dirname);
+      expect(solution.part1(input)).toBe(0);
+    });
+  });
+
+  describe("part 2", () => {
+    test("part 2 example", () => {
+      const input = getExampleInput(__dirname);
+      expect(solution.part2(input)).toBe(0);
+    });
+    test("part 2 input", () => {
+      const input = getInput(__dirname);
+      expect(solution.part2(input)).toBe(0);
+    });
+  });
+});
