9e7c2d4560401bd35f373180425de8f883cda921
Bart Kardol <bart.kardol@essent.nl>
2024-12-19 06:43:04 +0100

    day19

--- src/ts/2024/day19/solution.spec.ts
+++ src/ts/2024/day19/solution.spec.ts
@@ -9,24 +9,24 @@ describe("Day 19", () => {
   });
 
   describe("part 1", () => {
-    test("part 1 example", () => {
+    test("part 1 example", async () => {
       const input = getExampleInput(__dirname);
-      expect(solution.part1(input)).toBe(0);
+      expect(solution.part1(input)).toBe(6);
     });
-    test("part 1 input", () => {
+    test("part 1 input", async () => {
       const input = getInput(__dirname);
-      expect(solution.part1(input)).toBe(0);
+      expect(solution.part1(input)).toBe(344);
     });
   });
 
   describe("part 2", () => {
     test("part 2 example", () => {
       const input = getExampleInput(__dirname);
-      expect(solution.part2(input)).toBe(0);
+      expect(solution.part2(input)).toBe(16);
     });
     test("part 2 input", () => {
       const input = getInput(__dirname);
-      expect(solution.part2(input)).toBe(0);
+      expect(solution.part2(input)).toBe(996172272010026);
     });
   });
 });
--- src/ts/2024/day19/solution.ts
+++ src/ts/2024/day19/solution.ts
@@ -1,9 +1,55 @@
+import { asStringArray } from "@aoc/common";
+
 export class Solution {
   public part1(input: string): number {
-    return 0;
+    const inputLines = asStringArray(input);
+    const availablePatterns = inputLines[0].split(", ");
+    const designs = inputLines.slice(2);
+
+    return designs.filter(
+      (design) =>
+        this.checkDesign(design, availablePatterns, new Map<string, number>()) >
+        0
+    ).length;
   }
 
   public part2(input: string): number {
-    return 0;
+    const inputLines = asStringArray(input);
+    const availablePatterns = inputLines[0].split(", ");
+    const designs = inputLines.slice(2);
+
+    return designs.reduce(
+      (acc, design) =>
+        acc +
+        this.checkDesign(design, availablePatterns, new Map<string, number>()),
+      0
+    );
+  }
+
+  private checkDesign(
+    design: string,
+    patterns: string[],
+    cache: Map<string, number>
+  ): number {
+    if (design.length === 0) {
+      return 1;
+    }
+
+    const cacheResult = cache.get(design);
+    if (cacheResult !== undefined) {
+      return cacheResult;
+    }
+
+    const result = patterns
+      .filter((pattern) => design.startsWith(pattern))
+      .reduce(
+        (acc, pattern) =>
+          acc + this.checkDesign(design.slice(pattern.length), patterns, cache),
+        0
+      );
+
+    cache.set(design, result);
+
+    return result;
   }
 }
