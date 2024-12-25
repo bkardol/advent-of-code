9eddd49624b74a0d94edceda8a80071f1203704f
Bart Kardol <bart.kardol@essent.nl>
2024-12-25 12:10:01 +0100

    day25

--- src/ts/2024/day25/solution.spec.ts
+++ src/ts/2024/day25/solution.spec.ts
@@ -11,22 +11,11 @@ describe("Day 25", () => {
   describe("part 1", () => {
     test("part 1 example", () => {
       const input = getExampleInput(__dirname);
-      expect(solution.part1(input)).toBe(0);
+      expect(solution.part1(input)).toBe(3);
     });
     test("part 1 input", () => {
       const input = getInput(__dirname);
-      expect(solution.part1(input)).toBe(0);
-    });
-  });
-
-  describe("part 2", () => {
-    test("part 2 example", () => {
-      const input = getExampleInput(__dirname);
-      expect(solution.part2(input)).toBe(0);
-    });
-    test("part 2 input", () => {
-      const input = getInput(__dirname);
-      expect(solution.part2(input)).toBe(0);
+      expect(solution.part1(input)).toBe(2618);
     });
   });
 });
--- src/ts/2024/day25/solution.ts
+++ src/ts/2024/day25/solution.ts
@@ -1,9 +1,37 @@
+import { asStringArray } from "@aoc/common";
+
 export class Solution {
   public part1(input: string): number {
-    return 0;
-  }
+    const keys: number[][] = [];
+    const locks: number[][] = [];
+    const maxHeight = 6;
+    input.split("\n\n").forEach((data) => {
+      const dataArray = asStringArray(data);
+      const isLock = dataArray[0] === "#####";
+      const sizes: number[] = [];
+      const height = maxHeight;
+      for (let i = 0; i < height; i++) {
+        for (let j = 1; j < dataArray.length; j++) {
+          const row = isLock ? j : dataArray.length - 1 - j;
+          if (dataArray[row][i] === ".") {
+            sizes.push(j - 1);
+            break;
+          }
+        }
+      }
+      if (isLock) {
+        locks.push(sizes);
+      } else {
+        keys.push(sizes);
+      }
+    });
 
-  public part2(input: string): number {
-    return 0;
+    return locks.reduce(
+      (acc, lock) =>
+        acc +
+        keys.filter((key) => key.every((size, i) => size + lock[i] < maxHeight))
+          .length,
+      0
+    );
   }
 }
